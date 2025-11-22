ab49be267860e4770ebd52d1ae9fd225a50fcc53
Shamsul Arifin <arifinbd6@gmail.com>
2025-11-23 03:26:48 +0600

    Added namespace example

---
+++ Namespace/Namespace.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+namespace first{
+    int x = 1;
+}
+
+namespace second{
+    int x = 2;
+}
+
+int main()
+{
+    /*
+    Namespace provides a solution for preventing name conflicts
+    in a large projects. Each entity needs a unique name.
+    A namespace allows for identically named entities as long
+    as the namespaces are different
+    */
+    using namespace first;
+
+    int x = 0;
+    // int x = 1;      Will cause redeclaration error after build
+
+    cout << x << endl;
+
+    return 0;
+}
