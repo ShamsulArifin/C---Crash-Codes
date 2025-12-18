d9b726d64ecbaf166f75d41cd3c1b35384d5ae01
Shamsul Arifin <arifinbd6@gmail.com>
2025-12-18 15:29:36 +0600

    Added while loop example

--- 17. While Loops/WhileLoops.cpp
+++ 17. While Loops/WhileLoops.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
     using std::cout, std::cin, std::string, std::endl;
+
+    string name;
+
+    while(name.empty())
+    {
+        cout << "Enter your name: ";
+        std::getline(cin, name);
+    }
+
+    // while(1==1)
+    // {
+    //     cout << "Help I'm stuck in an infinite loop";
+    // }
+
+    cout << "Hello " << name;
 
     return 0;
 }
