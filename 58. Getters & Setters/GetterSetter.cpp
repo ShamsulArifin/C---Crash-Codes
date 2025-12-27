ca978982cfed77b69dc597427eeedf8b23bfce1c
Shamsul Arifin <arifinbd6@gmail.com>
2025-12-27 08:19:31 +0600

    Added getters and setters example

---
+++ 58. Getters & Setters/GetterSetter.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+class Stove
+{
+private:
+  int temperature = 0;
+
+public:
+  Stove(int temperature)
+  {
+    setTemperature(temperature);
+  }
+
+  int getTemperature()
+  {
+    return temperature;
+  }
+  void setTemperature(int temperature)
+  {
+    if (temperature < 0)
+    {
+      this->temperature = 0;
+    }
+    else if (temperature >= 10)
+    {
+      this->temperature = 10;
+    }
+    else
+    {
+      this->temperature = temperature;
+    }
+  }
+};
+
+int main()
+{
+  using std::cout, std::cin, std::string, std::endl;
+
+  // Abstraction = hiding unnecessary data from outside a class
+  // getter = function that makes a private attribute READABLE
+  // setter = function that makes a private attribute WRITEABLE
+
+  Stove stove(0);
+  // stove.setTemperature(1000000);
+
+  cout << "The temperature setting is: " << stove.getTemperature();
+
+  return 0;
+}
